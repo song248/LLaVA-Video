5ec90ef28530c41dbf0aa26fb6cb956dc21cd19b
song248 <songteagyong@gmail.com>
2025-07-14 11:49:50 +0900

    feat: fine tune

--- fine_tune.sh
+++ fine_tune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1
-export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.75,max_split_size_mb:32
+export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.7,max_split_size_mb:64
 
 # Set up the data folder
 VIDEO_FOLDER="data"
@@ -11,7 +11,8 @@ DATA_YAML="labeled.json"
 # alias python=python3
 
 ################ Job Config ################
-LLM_VERSION="Qwen/Qwen1.5-1.8B"
+# LLM_VERSION="Qwen/Qwen1.5-1.8B"
+LLM_VERSION="Qwen/Qwen3-1.7B"
 LLM_VERSION_CLEAN="${LLM_VERSION//\//_}"
 VISION_MODEL_VERSION="google/siglip-so400m-patch14-384"
 VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
@@ -19,7 +20,8 @@ VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
 BASE_RUN_NAME="llava-lora-fight-vqa"
 RUN_NAME="${BASE_RUN_NAME}-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}"
 # PREV_STAGE_CHECKPOINT="lmms-lab/LLaVA-Video-7B-Qwen2"
-PREV_STAGE_CHECKPOINT="Qwen/Qwen1.5-1.8B"
+# PREV_STAGE_CHECKPOINT="Qwen/Qwen1.5-1.8B"
+PREV_STAGE_CHECKPOINT="Qwen/Qwen3-1.7B"
 
 echo "RUN_NAME: ${RUN_NAME}"
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
@@ -45,35 +47,34 @@ deepspeed --master_port 30000 \
     --fp16 False\
     --run_name $RUN_NAME \
     --output_dir ./work_dirs/$RUN_NAME \
-    --num_train_epochs 2 \
-    --per_device_train_batch_size 1 \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --save_strategy "steps" \
     --save_steps 500 \
     --save_total_limit 1 \
-    --learning_rate 2e-6 \
+    --learning_rate 5e-6 \
     --weight_decay 0.01 \
-    --warmup_ratio 0.1 \
-    --lr_scheduler_type "linear" \
+    --warmup_ratio 0.2 \
+    --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 1024 \
-    --gradient_checkpointing False \
-    --dataloader_num_workers 2 \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to none \
     --torch_compile False \
-    --torch_compile_backend "inductor" \
     --dataloader_drop_last True \
     --frames_upbound 128 \
     --mm_newline_position grid \
     --add_time_instruction True \
     --force_sample True \
-    --mm_spatial_pool_stride 2 \
+    --mm_spatial_pool_stride 1 \
     --lora_enable True \
-    --lora_r 8 \
-    --lora_alpha 16 \
-    --lora_dropout 0.05 \
+    --lora_r 16 \
+    --lora_alpha 32 \
+    --lora_dropout 0.1 \
     --lora_bias "none"
 exit 0;
